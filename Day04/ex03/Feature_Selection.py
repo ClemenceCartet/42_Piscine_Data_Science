53a78201ac299f37debe541b7cbad039fa713b1e
Clémence Cartet <87325523+ClemenceCartet@users.noreply.github.com>
2024-06-16 16:12:14 +0200

    Add files via upload

--- Day04/ex03/Feature_Selection.py
+++ Day04/ex03/Feature_Selection.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
+from sklearn.linear_model import LinearRegression
 
 
 def load(path: str):
@@ -19,23 +18,78 @@ def load(path: str):
         return None
 
 
-def standardize(data):
-    for col in data.columns:
-        if not pd.api.types.is_string_dtype(data[col]):
-            data[col] = (
-                data[col].subtract(data[col].mean()).divide(data[col].std())
-            )
+def train_to_find_vif(data):
+    features = data.columns
+    vif_dict: dict = {}
+    for feature in features:
+        y = data[feature]
+        x = data.drop(columns=feature)
+        model = LinearRegression().fit(x, y)
+        r_squared = model.score(x, y)
+        tolerance = round((1 - r_squared), 6)
+        vif = round((1 / tolerance), 6)
+        vif_dict[feature] = (vif, tolerance)
+
+    df = pd.DataFrame.from_dict(vif_dict, orient="index")
+    df.columns = ["VIF", "Tolerance"]
+
+    return df
 
 
 def main():
     """Main function"""
     data = load("Test_knight.csv")
-    # data.drop(columns="knight", inplace=True)
     if data is not None:
-        data_var = data.var()
+        all_features = train_to_find_vif(data)
+        all_features_sorted = all_features.sort_values(by="VIF", ascending=False)
+        print(all_features_sorted)
 
+        # test1 = data.drop(
+        #     columns=[
+        #         "Slash",
+        #         "Pull",
+        #         "Sensitivity",
+        #         "Recovery",
+        #         "Awareness",
+        #         "Prescience",
+        #         "Delay",
+        #         "Dexterity",
+        #         "Empowered",
+        #         "Attunement",
+        #     ]
+        # )
+        # all_features = train_to_find_vif(test1)
+        # all_features_sorted = all_features.sort_values(by="VIF", ascending=False)
+        # print(all_features_sorted)
 
-        # plt.show()
+        test2 = data.drop(
+            columns=[
+                "Prescience",
+                "Push",
+                "Deflection",
+                "Survival",
+                "Midi-chlorien",
+                "Grasping",
+                "Pull",
+                "Awareness",
+                "Repulse",
+                "Attunement",
+                "Empowered",
+                "Dexterity",
+                "Delay",
+                "Slash",
+                "Sprint",
+                "Sensitivity",
+                "Stims",
+                "Strength",
+                "Recovery",
+                "Hability",
+                "Agility"
+            ]
+        )
+        all_features = train_to_find_vif(test2)
+        all_features_sorted = all_features.sort_values(by="VIF", ascending=False)
+        print(all_features_sorted)
 
 
 if __name__ == "__main__":
---
+++ Day04/ex04/Tree.py
@@ -0,0 +1,53 @@
+import os
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn import tree
+import matplotlib.pyplot as plt
+
+
+def load(path: str):
+    """Load a data file using pandas library"""
+    try:
+        assert isinstance(path, str), "your path is not valid."
+        assert os.path.exists(path), "your file doesn't exist."
+        assert os.path.isfile(path), "your 'file' is not a file."
+        assert path.lower().endswith(".csv"), "file format is not .csv."
+        data = pd.read_csv(path)
+        print(f"Loading dataset of dimensions {data.shape}")
+        return data
+    except AssertionError as msg:
+        print(f"{msg.__class__.__name__}: {msg}")
+        return None
+
+
+def main():
+    """Main function"""
+    train_knight = load("Train_knight.csv")
+    test_knight = load("Test_knight.csv")
+    if train_knight is not None and test_knight is not None:
+        x = train_knight.drop(columns='knight')
+        y = train_knight['knight']
+        x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=42)
+
+        model = DecisionTreeClassifier(random_state=13)
+        model.fit(x_train, y_train)
+        predicted_val = model.predict(x_val)
+        accuracy = accuracy_score(predicted_val, y_val)
+        print(f"Accuracy: {accuracy}")
+        print(f"F1_score: {round(f1_score(y_val, predicted_val, average='macro'), 4)}")
+
+        predicted_test = model.predict(test_knight)
+        with open("Tree.txt", "w") as output:
+            for item in predicted_test:
+                output.write(item + "\n")
+
+        plt.figure(figsize=(10, 7))
+        tree.plot_tree(model, feature_names=test_knight.columns,
+					class_names=['Jedi', 'Sith'], filled=True)
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()
